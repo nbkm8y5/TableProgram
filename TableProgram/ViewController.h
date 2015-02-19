ed862dd6c6e138c12f2a7baf869d843dbbace48a
nbkm8y5 <moreno.ronline@gmail.com>
2015-02-19 17:35:15 -0500

    First Table Program on Objective C

---
+++ TableProgram/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TableProgram
+//
+//  Created by Rolando Moreno on 2/19/15.
+//  Copyright (c) 2015 Rolando Moreno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
